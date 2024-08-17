5dc19962779d5c39a16248476d06d290670571ab
Bhoomi1503 <bhoomiya1503@gmail.com>
2024-08-17 12:03:34 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import ReactDOM from 'react-dom';
+import React,{useState,useEffect} from 'react';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Userdemo(){
+  const[posts,setUsers]=useState([]);
+  const[loading,setLoading]=useState(true);
+  useEffect(()=>{
+      fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(data =>{setUsers(data)
+          setLoading(false);
+        })
+        
+      .catch(error=>{
+        console.error("if it is not loading, will be error message");
+        setLoading(false)
+      })
+  },[])
+  if(loading){
+    return <p>Loading</p>;
+  }
+  return (
+    <div>
+      <ul>
+      <h1>List out the users in API</h1>
+        {posts.map(post=>(
+          <li key={post.id}>Id:{post.id}<br/>Title:{post.title}<br/>Body:{post.body}</li>         
+        ))}
+      </ul>
+    </div>
+  );
+}
+ReactDOM.render(<Userdemo/>,document.getElementById('root'));
+
